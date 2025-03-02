e78faf8dc7013c1dd1e60580fbb62077894e7cf3
Leonardo Marcatti da Silva <leonardomarcatti@hotmail.com>
2025-03-01 23:14:56 -0300

    upgrades

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,7 +10,6 @@ class PostsController extends Controller
     public function create(Request $r): object
     {
         $post = $r->all();
-        \dd($post);
         $model = new PostsModel($post);
         $status = $model->save();
         if ($status) {
